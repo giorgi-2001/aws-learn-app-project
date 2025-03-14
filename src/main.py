935cc84832805116a8e97261c8868cb1fe61465b
Giorgi Ananiashvili <gio19900504@gmail.com>
2025-03-14 18:01:32 +0400

    Automated deployment

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+import requests
 
 
 app  = FastAPI()
 
 
+METADATA_URL = "http://169.254.169.254/latest/meta-data/"
+
+
+def get_metadata(path):
+    """ Helper function to fetch instance metadata """
+    try:
+        return requests.get(METADATA_URL + path).text
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return "Unavailable"
+
+
 @app.get("/")
-def greeting():
-    return {"greeting": "Hello, world"}
+def get_region_and_AZ():
+    availability_zone = get_metadata("placement/availability-zone")
+    region = availability_zone[:-1] 
+
+    return {
+        "region": region,
+        "az": availability_zone
+    }
